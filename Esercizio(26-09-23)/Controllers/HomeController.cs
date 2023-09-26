6c1cd668a42aa957e64466b6ed877ced8b14a1cf
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-09-26 12:50:35 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio(26-09-23)/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Esercizio_26_09_23_.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Esercizio_26_09_23_.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Esercizio(26-09-23)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Esercizio_26_09_23_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
